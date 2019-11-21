ee4eb979d7a49b75b6ba9164e5ff6147b6420e0a
DhaminiDham <dhamini.bandaru@gmail.com>
2019-11-21 11:32:10 +0530

    4 files modified, 1 added, 1 deleted

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-//import { environment } from './environments/environment';
+import { environment } from './environments/environments';
 
 import { AppModule } from './app/app.module';
 
-
-/*if (environment.production) {
+if (environment.production) {
   enableProdMode();
-}*/
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
 
